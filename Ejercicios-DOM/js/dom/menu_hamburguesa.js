1962cec058ef495eb9d497d8f9d3f85913919760
dannyarnold <dannyarnold2006@hotmail.com>
2024-09-02 23:00:48 -0300

    ejercicio práctica js 81

--- Ejercicios-DOM/js/dom/menu_hamburguesa.js
+++ Ejercicios-DOM/js/dom/menu_hamburguesa.js
@@ -1,1 +1,19 @@
-const menu = document.querySelector('');
+export default function hamburguerMenu(panelBtn, panel, menuLink) {
+  const d = document;
+
+  d.addEventListener("click", (e) => {
+    if (e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)) {
+      d.querySelector(panel).classList.toggle("is-active");
+      d.querySelector(panelBtn).classList.toggle("is-active");
+    }
+    if (e.target.matches(menuLink)) {
+      d.querySelector(panel).classList.remove("is-active");
+      d.querySelector(panelBtn).classList.remove("is-active");
+    }
+  });
+  // d.addEventListener("DOMContentLoaded", (e) => {
+  //   hamburgerMenu(".panel-btn", ".panel");
+  //   hamburgerMenu(".hamburger-box", ".panel");
+  //   hamburgerMenu(".hamburger-inner", ".panel");
+  // });
+}
