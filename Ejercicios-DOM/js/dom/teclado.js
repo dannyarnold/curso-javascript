969c912795147cfaf08a17bce0a7356ad4655b11
dannyarnold <dannyarnold2006@hotmail.com>
2024-09-08 10:52:01 -0300

    Actualizacion repo

---
+++ Ejercicios-DOM/js/dom/teclado.js
@@ -0,0 +1,54 @@
+const d = document;
+
+let x = 0,
+  y = 0;
+
+export function moveBall(e, ball, stage) {
+  const $ball = d.querySelector(ball),
+    $stage = d.querySelector(stage);
+  console.log(e.keyCode);
+
+  const move = (direction) => {};
+  console.log(e.key);
+  switch (e.keyCode) {
+    case 37:
+      // move("left");
+      x--;
+      break;
+    case 38:
+      // move("up");
+      y--;
+      break;
+    case 39:
+      // move("right");
+      x++;
+      break;
+    case 40:
+      // move("down");
+      y++;
+      break;
+    default:
+      break;
+
+    $ball.style.transform = `translate(${x * 10}px, ${y * 10}px)`;
+  }
+}
+export function shortcuts(e) {
+  console.log(e.type);
+  console.log(e.key);
+  console.log(e.keyCode);
+  console.log(`ctrl: ${e.ctrlKey}`);
+  console.log(`alt: ${e.altKey}`);
+  console.log(`shift: ${e.shiftKey}`);
+  console.log(e);
+
+  if (e.key === "a" && e.altKey) {
+    alert("Haz lanzado una alerta con el teclado");
+  }
+  if (e.key === "c" && e.altKey) {
+    confirm("Haz lanzado una confirmación con el teclado");
+  }
+  if (e.key === "p" && e.altKey) {
+    prompt("Haz lanzado un aviso con el teclado");
+  }
+}
--- Ejercicios-DOM/js/index_dom.js
+++ Ejercicios-DOM/js/index_dom.js
@@ -1,10 +1,23 @@
 import hamburguerMenu from "./dom/menu_hamburguesa.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
+import { moveBall,shortcuts } from "./dom/teclado.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
   hamburguerMenu(".panel-btn", ".panel", "menu a");
   digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
-  alarm("assets/alarma.mp3", "#activar-reloj", "#desactivar-reloj");
+  alarm("assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
+
 });
+
+
+d.addEventListener('keydown',e=>{
+  shortcuts(e);
+})
+d.addEventListener('keyup',e=>{
+  // shortcuts(e);
+})
+d.addEventListener('keypress',e=>{
+  // shortcuts(e);
+})
