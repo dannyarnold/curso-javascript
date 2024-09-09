5e9c46008a76f0e707fe37aa3cce2ce37b6e52f3
dannyarnold <dannyarnold2006@hotmail.com>
2024-09-09 17:56:03 -0300

    Actualizacion repo

--- Ejercicios-DOM/js/dom/cuenta_regresiva.js
+++ Ejercicios-DOM/js/dom/cuenta_regresiva.js
@@ -1,20 +1,34 @@
 const d = document;
-export default function countdown(id,limitDate,finalMessage){
-    const $countdown = document.getElementById(id),
+export default function countdown(id, limitDate, finalMessage) {
+  const $countdown = document.getElementById(id),
     countdownDate = new Date(limitDate).getTime();
 
-    let countdownTempo=setInterval(() => {
-        let now = new Date().getTime(),
-        limitTime=countdownDate-now,
-        days = 1000 * 60 * 60 * 24,
-        hours,
-        minutes,
-        seconds;
+  let countdownTempo = setInterval(() => {
+    let now = new Date().getTime(),
+      limitTime = countdownDate - now,
+      days = Math.floor(limitTime / (1000 * 60 * 60 * 24)),
+      hours = (
+        "0" +
+        // Math.floor(limitTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        Math.floor(limitTime % (24)) 
 
-        $countdown.innerHTML=`<h3>Faltan: ${days} días ${hours} horas ${minutes} minutos ${seconds} segundos</h3>`;
+      ).slice(-2),
+      minutes = (
+        "0" +
+        Math.floor(limitTime % (60))
+      ).slice(-2),
+      seconds = (
+        "0" +
+        Math.floor(limitTime )
+      ).slice(-2);
 
-        console.log(countdownDate,new Date().getTime())
-    }, 1000);
+    $countdown.innerHTML = `<h3>Faltan: ${days} días ${hours} horas ${minutes} minutos ${seconds} segundos</h3>`;
 
+    console.log(countdownDate, new Date().getTime());
 
+    if(limitTime<0){
+      clearInterval(countdownTempo);
+      $countdown.innerHTML = `<h3>${finalMessage}</h3>`;
+    }
+  }, 1000);
 }
