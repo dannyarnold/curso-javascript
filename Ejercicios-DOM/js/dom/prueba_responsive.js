dd354bc306fc12a37d56064edae6ba0b6dc12e50
dannyarnold <dannyarnold2006@hotmail.com>
2024-10-12 22:05:17 -0300

    Actualizacion repo

--- Ejercicios-DOM/js/dom/prueba_responsive.js
+++ Ejercicios-DOM/js/dom/prueba_responsive.js
@@ -1,3 +1,12 @@
 const d = document;
 
-export default function responsiveTester(id) {}
+export default function responsiveTester(form) {
+    const $form = d.getElementById(form);
+    let tester;
+
+    d.addEventListener('submit', (e) => {
+        if (e.target === $form) {
+            alert('Formulario enviado');
+        }
+    });
+}
